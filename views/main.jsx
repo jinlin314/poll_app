245709517f2b393418be49eaad04ebacaf9d5d28
Jin Lin <jinlin314@hotmail.com>
2017-10-18 18:09:49 -0400

    react setup added, boostrap & custom css added

---
+++ views/main.jsx
@@ -0,0 +1,13 @@
+'use strict'
+import React from 'react'
+import ReactDom from 'react-dom'
+import App from './App'
+
+ReactDom.render(
+  <App />,
+  document.getElementById('app')
+);
+
+
+//    "start": "nodemon -L app.js",
+
